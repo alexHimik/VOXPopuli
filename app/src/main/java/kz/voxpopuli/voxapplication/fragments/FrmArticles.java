451a1836c71200622127427f62670c1eaf0fc422
jura <jura@pisem.net>
2015-04-09 14:21:11 +0300

    Merge branch 'master' of https://github.com/alexHimik/VOXPopuli
    Conflicts:
    	app/src/main/res/values/themes.xml

--- app/src/main/java/kz/voxpopuli/voxapplication/fragments/FrmArticles.java
+++ app/src/main/java/kz/voxpopuli/voxapplication/fragments/FrmArticles.java
@@ -1,7 +1,21 @@
 package kz.voxpopuli.voxapplication.fragments;
 
-/**
- * Created by user on 09.04.15.
- */
-public class FrmArticles {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kz.voxpopuli.voxapplication.R;
+
+public class FrmArticles extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.articles, container,
+                false);
+
+        return rootView;
+    }
 }
