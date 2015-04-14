2fd9c3276fb1febff175aad8853b9f066c108ee8
alexhimik <hire.himik@gmail.com>
2015-04-14 12:57:07 +0300

    adapter reverted to the actual state

--- app/src/main/java/kz/voxpopuli/voxapplication/adapter/ArticlesAdapter.java
+++ app/src/main/java/kz/voxpopuli/voxapplication/adapter/ArticlesAdapter.java
@@ -1,9 +1,7 @@
 package kz.voxpopuli.voxapplication.adapter;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,26 +11,28 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.List;
+
 import kz.voxpopuli.voxapplication.R;
-import kz.voxpopuli.voxapplication.model.ModelArticle;
+import kz.voxpopuli.voxapplication.network.wrappers.mpage.Article;
 
 public class ArticlesAdapter extends BaseAdapter {
     private Fragment context;
-    private ModelArticle[] items;
+    private List<Article> items;
 
-    public ArticlesAdapter(Fragment context, ModelArticle[] items) {
+    public ArticlesAdapter(Fragment context, List<Article> items) {
         this.context = context;
         this.items = items;
     }
 
     @Override
     public int getCount() {
-        return items == null ? 0 : items.length;
+        return items == null ? 0 : items.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return items == null ? null : items[position];
+        return items == null ? null : items.get(position);
     }
 
     @Override
@@ -61,10 +61,8 @@ public class ArticlesAdapter extends BaseAdapter {
         TextView txt_views;
         TextView txt_comment;
 
-//        Bitmap bm = Bitmap.createBitmap(R.dimen.img_w_l, R.dimen.img_h_l, Bitmap.Config.RGB_565);
-
         View view = convertView;
-        ModelArticle ma = items[position];
+        Article ma = items.get(position);
         switch (getItemViewType( position)){
             case 0: view = ((LayoutInflater) context.getActivity().getSystemService(
                     Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.item_red, null);
@@ -76,7 +74,6 @@ public class ArticlesAdapter extends BaseAdapter {
                     Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.item_little, null);
         }
 
-
         switch (getItemViewType( position)){
             case 0:
                 iv = (ImageView) view.findViewById(R.id.imv);
@@ -87,16 +84,12 @@ public class ArticlesAdapter extends BaseAdapter {
                 txt_comment = (TextView) view.findViewById(R.id.txt_comment);
                 descr.setText(ma.getDescription());
                 Glide.with(context).
-                        load(ma.getImage_mid()).
+                        load(ma.getImageMid()).
                         into(iv);
-//                Glide.with(context).
-//                        load(ma.getImage_mid()).
-//                        into(bm);
-//                iv.setImageBitmap(cut(bm,R.dimen.img_w_l, R.dimen.img_h_l));
                 title.setText(ma.getTitle());
-                date.setText(ma.getDate());
-                txt_views.setText(""+ma.getViews());
-                txt_comment.setText(""+ma.getComment());
+                date.setText(ma.getPostDate());
+                txt_views.setText(ma.getViwed());
+                txt_comment.setText(ma.getCommentsAmount());
                 break;
             case 1:
                 iv = (ImageView) view.findViewById(R.id.imv_w);
@@ -104,11 +97,11 @@ public class ArticlesAdapter extends BaseAdapter {
                 date = (TextView) view.findViewById(R.id.date_w);
                 txt_views = (TextView) view.findViewById(R.id.txt_views_w);
                 txt_comment = (TextView) view.findViewById(R.id.txt_comment_w);
-                Glide.with(context).load(ma.getImage_mid()).into(iv);
+                Glide.with(context).load(ma.getImageMid()).into(iv);
                 title.setText(ma.getTitle());
-                date.setText(ma.getDate());
-                txt_views.setText(""+ma.getViews());
-                txt_comment.setText(""+ma.getComment());
+                date.setText(ma.getPostDate());
+                txt_views.setText(ma.getViwed());
+                txt_comment.setText(ma.getCommentsAmount());
                 break;
             case 2:
                 iv = (ImageView) view.findViewById(R.id.imv_l);
@@ -116,39 +109,12 @@ public class ArticlesAdapter extends BaseAdapter {
                 date = (TextView) view.findViewById(R.id.date_l);
                 txt_views = (TextView) view.findViewById(R.id.txt_views_l);
                 txt_comment = (TextView) view.findViewById(R.id.txt_comment_l);
-                Glide.with(context).load(ma.getImage_mid()).into(iv);
+                Glide.with(context).load(ma.getImageMid()).into(iv);
                 title.setText(ma.getTitle());
-                date.setText(ma.getDate());
-                txt_views.setText(""+ma.getViews());
-                txt_comment.setText(""+ma.getComment());
+                date.setText(ma.getPostDate());
+                txt_views.setText(ma.getViwed());
+                txt_comment.setText(ma.getCommentsAmount());
         }
-
         return view;
     }
-
-//    public Bitmap cut (Bitmap bitmap, int reqWidth, int reqHeight){
-//        Bitmap resBitmap;
-//        int wIm, hIm, wBegin,hBegin;
-//        int width = bitmap.getWidth();
-//        int height = bitmap.getHeight();
-//        float w1=(float) width/reqWidth;
-//        float h1=(float) height/reqHeight;
-//        if (w1>=h1) {        // обрезаем ширину width
-//            wIm = (int) (reqWidth*h1);
-//            hIm = height;
-//            wBegin = (width - wIm) / 2;
-//            hBegin = 0;
-//        } else {        // обрезаем высоту height
-//            wIm = width;
-//            hIm = (int) (reqHeight * w1);
-//            hBegin = (height - hIm) / 2;
-//            wBegin = 0;
-//        }
-//        resBitmap = Bitmap.createBitmap(wIm, hIm, Bitmap.Config.RGB_565);
-//        int[] pixels = new int[wIm * hIm];
-//        bitmap.getPixels(pixels, 0, wIm, wBegin, hBegin, wIm, hIm);
-//        resBitmap.setPixels(pixels, 0, wIm, 0, 0, wIm, hIm);
-//        return resBitmap;
-//    }
-
 }
